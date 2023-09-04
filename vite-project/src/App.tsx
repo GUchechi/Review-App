56f8a6939f7a0b00e80490537a7a062df19b99e9
GUchechi <godswilluchendu7@gmail.com>
2023-09-04 18:51:00 +0100

    added review-stats

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import "./App.css";
 import FeedbackForm from "./components/FeedbackForm/FeedbackForm";
+import ReviewStats from "./components/ReviewStats/ReviewStats";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header title="Review App" />
       <div className="container">
         <FeedbackForm />
+        <ReviewStats />
       </div>
     </>
   );
---
+++ vite-project/src/components/ReviewStats/ReviewStats.tsx
@@ -0,0 +1,12 @@
+import "./ReviewStats.css";
+
+const ReviewStats = () => {
+  return (
+    <div className="review-stats">
+      <h4>10 Reviews</h4>
+      <h4>Average Rating:10</h4>
+    </div>
+  );
+};
+
+export default ReviewStats;
