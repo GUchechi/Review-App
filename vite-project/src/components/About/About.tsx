3cbe6a53b3e228072eb04c335a1087e45c81b3eb
GUchechi <godswilluchendu7@gmail.com>
2023-09-04 17:07:58 +0100

    added card Components

--- vite-project/src/components/About/About.tsx
+++ vite-project/src/components/About/About.tsx
@@ -1,17 +1,21 @@
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
+import Card from "../shared/Card/Card";
+
 
 const About = (): ReactElement => {
   return (
-    <div className="about">
-      <h1>About This Project</h1>
-      <p>This is a React app to leave review for a product or service</p>
-      <p>Version: 1.0.0</p>
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React app to leave review for a product or service</p>
+        <p>Version: 1.0.0</p>
 
-      <p>
-        <Link to="/">Back To Home</Link>
-      </p>
-    </div>
+        <p>
+          <Link to="/">Back To Home</Link>
+        </p>
+      </div>
+    </Card>
   );
 };
 
