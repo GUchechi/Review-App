ccf2599cad492b5237e8a09bea95ba7c8f5c0896
GUchechi <godswilluchendu7@gmail.com>
2023-09-06 17:19:53 +0100

    reviewList

--- vite-project/src/components/ReviewItem/ReviewItem.tsx
+++ vite-project/src/components/ReviewItem/ReviewItem.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+// import { useContext } from "react";
+import { FaEdit, FaTimes } from "react-icons/fa";
+import Card from "../shared/Card/Card";
+import './ReviewItem.css'
 
-const ReviewItem = () => {
+type ReviewItemProps = {
+  id: number;
+  rating: number;
+  text: string;
+};
+
+const ReviewItem = ({ item }: { item: ReviewItemProps }) => {
   return (
-    <div>ReviewItem</div>
-  )
-}
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes color="purple" />
+      </button>
+      <button className="edit">
+        <FaEdit color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
+};
 
-export default ReviewItem
+export default ReviewItem;
--- vite-project/src/components/ReviewList/ReviewList.tsx
+++ vite-project/src/components/ReviewList/ReviewList.tsx
@@ -1,29 +1,35 @@
-import { ReactElement, useContext } from "react";
+import { useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import ReviewContext from "../../context/ReviewContext";
 import Spinner from "../shared/Spinner/Spinner";
 import ReviewItem from "../ReviewItem/ReviewItem";
 
+type ReviewItemProps = {
+  id: number;
+  rating: number;
+  text: string;
+};
+
 const ReviewList = () => {
   const { review, isLoading } = useContext(ReviewContext);
 
   if (!isLoading && (!review || review.length === 0)) {
-    return <p>There is no Review</p>;
+    return <p>There are no Reviews</p>;
   }
 
   return isLoading ? (
     <Spinner />
   ) : (
-    <div className="feedback-list">
+    <div className="review-list">
       <AnimatePresence>
-        {review.map((item) => (
+        {review.map((item: ReviewItemProps) => (
           <motion.div
             key={item.id}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <ReviewItem key={item.id} item={item} />
+            <ReviewItem item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
