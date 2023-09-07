9ad059395ce5b0ad04df0ea96b587facb9a94bb8
GUchechi <godswilluchendu7@gmail.com>
2023-09-07 09:26:21 +0100

    refactoring

--- vite-project/src/components/ReviewStats/ReviewStats.tsx
+++ vite-project/src/components/ReviewStats/ReviewStats.tsx
@@ -1,10 +1,23 @@
+import { useContext } from "react";
 import "./ReviewStats.css";
+import ReviewContext from "../../context/ReviewContext";
+
 
 const ReviewStats = () => {
+  const {review} = useContext(ReviewContext)
+
+  // Calculate Ratings Average
+  let ratingAverage = review.reduce((acc,cur) => {
+    return acc + cur.rating;
+  }, 0) / review.length;
+
+  ratingAverage = ratingAverage.toFixed(0).replace(/[.,]0$/, "");
+
+
   return (
     <div className="review-stats">
-      <h4>10 Reviews</h4>
-      <h4>Average Rating:10</h4>
+      <h4>{review.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(ratingAverage) ? 0 : ratingAverage}</h4>
     </div>
   );
 };
